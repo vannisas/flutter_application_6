bfac5b40732aff0043167b6f115adeb5bb88bed7
tvo03 <143240295+tvo03@users.noreply.github.com>
2025-02-10 19:14:45 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Vannisa'),
+      home: const MyHomePage(title: 'Vannisa and Tien'),
     );
   }
 }
